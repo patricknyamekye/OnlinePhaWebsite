33e157c54aac59697408c911e9140d7d04bb142c
patricknyamekye <125988403+patricknyamekye@users.noreply.github.com>
2025-06-21 11:08:13 -1200

    fa icons done

--- client/src/components/NavbarComponents/NavbarComponents.jsx
+++ client/src/components/NavbarComponents/NavbarComponents.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import styles from "./Navbar.module.css";
 import pillsLogo from "../../assets/pills.png";
+import { FaHome, FaBox, FaPhone, FaSignInAlt } from "react-icons/fa";import { LiaShoppingCartSolid } from "react-icons/lia"
+import { RiUploadCloudLine } from "react-icons/ri";
+import { LiaUserInjuredSolid } from "react-icons/lia";
+
+
 
 const NavbarComponents = () => {
   return (
@@ -12,10 +17,18 @@ const NavbarComponents = () => {
           
          </div>
       <ul className={styles.navLinks}>
-        <li><a href="/home">Home</a></li>
-        <li><a href="/product">Product</a></li>
-        <li><a href="/contact">Contact</a></li>
-        <li><a href="/login">Login</a></li>
+        <li><a href="/home"> <FaHome/>Home</a></li>
+        <li><a href="/product"><FaBox/> Product</a></li>
+        <li><a href="/contact"> Contact <FaPhone/></a></li>
+        <li><a href="/contact"> Services <LiaUserInjuredSolid /></a></li>
+        <li><a href="/contact"> About Us<FaPhone/></a></li>
+        <li><a href="/login"> Login <FaSignInAlt/></a></li>
+      
+      </ul>
+
+      <ul className={styles.ups}>
+          <li><a href="/Cards"> <LiaShoppingCartSolid /></a></li>
+          <li><a href="/Cards"> <RiUploadCloudLine /></a></li>
       </ul>
     </nav>
   )
