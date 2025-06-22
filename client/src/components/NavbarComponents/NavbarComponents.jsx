ac00aaf1ee5e8eaa08a4a7ae60ed6fe2cb4c1a8b
patricknyamekye <125988403+patricknyamekye@users.noreply.github.com>
2025-06-22 00:09:27 -1200

    responsive for tablet

--- client/src/components/NavbarComponents/NavbarComponents.jsx
+++ client/src/components/NavbarComponents/NavbarComponents.jsx
@@ -1,22 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "./Navbar.module.css";
 import pillsLogo from "../../assets/pills.png";
 import { FaHome, FaBox, FaPhone, FaSignInAlt } from "react-icons/fa";import { LiaShoppingCartSolid } from "react-icons/lia"
 import { RiUploadCloudLine } from "react-icons/ri";
 import { LiaUserInjuredSolid } from "react-icons/lia";
+import { TfiMenu } from "react-icons/tfi";
 
 
 
 const NavbarComponents = () => {
+
+  const [menuOpen, setMenuOpen] = useState(false );
+
+  const handleMenuClick = () => {
+    setMenuOpen(!menuOpen);
+  }
+
+
   return (
  <nav className={styles.navbar}>
       <div className={styles.logo}>
        
         <span><img src= {pillsLogo} alt="" /></span>
-         <li>WellCare     </li> 
+         <li className={styles.logoname}>WellCare     </li> 
           
          </div>
-      <ul className={styles.navLinks}>
+      <ul className={` ${styles.navLinks}  ${menuOpen ? styles.showMenu : "" }` }>
         <li><a href="/home"> <FaHome/>Home</a></li>
         <li><a href="/product"><FaBox/> Product</a></li>
         <li><a href="/contact"> Contact <FaPhone/></a></li>
@@ -29,6 +38,7 @@ const NavbarComponents = () => {
       <ul className={styles.ups}>
           <li><a href="/Cards"> <LiaShoppingCartSolid /></a></li>
           <li><a href="/Cards"> <RiUploadCloudLine /></a></li>
+          <li className={styles.menu}  onClick={handleMenuClick}> <TfiMenu /></li>
       </ul>
     </nav>
   )
