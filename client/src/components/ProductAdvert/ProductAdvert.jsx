d994f6338ce302a3fe8a35b00d13ed4374b7d884
patricknyamekye <125988403+patricknyamekye@users.noreply.github.com>
2025-07-12 08:56:40 -1200

    productAvert

--- client/src/components/ProductAdvert/ProductAdvert.jsx
+++ client/src/components/ProductAdvert/ProductAdvert.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
 import styles from './ProductAdvert.module.css';
+import bgImage from '../../assets/PdA.png'; // ✅ corrected path
 
 
 const ProductAdvert = () => {
   return (
     <div className={styles.advertContainer}>
-       <div className={styles.advertWrapper}>
-        hello
-       </div>
+      <div className={styles.wrapper}>
 
-    
-    
+       <div className={styles.imageContainer}>
+          <img src={bgImage} alt="product" />
+        </div>
+        <div className={styles.PdAText}>
+          <span className={styles.label}>FOR WOMEN</span>
+          <h1>All Natural Promotes <br /> Focus and Memory</h1>
+          <button>Shop Now</button>
+        </div>
+
+       
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductAdvert
+export default ProductAdvert;
